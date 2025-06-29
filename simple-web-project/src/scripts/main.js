09e8c4d9f2e23d01c8224cb5213a2fc27b5445d4
astikspandey <147242239+astikspandey@users.noreply.github.com>
2025-06-29 11:38:47 +0530

    Update main.js

--- simple-web-project/src/scripts/main.js
+++ simple-web-project/src/scripts/main.js
@@ -1,1 +1,2 @@
-window.location.href = "https://2ef7-122-171-17-120.ngrok-free.app";
+#window.location.href = "https://2ef7-122-171-17-120.ngrok-free.app";
+window.location.href = "https://maintenance.tiiny.site";
